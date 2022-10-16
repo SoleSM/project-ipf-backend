b52c94a1d8f4ac1ea323df86951269175a9fde07
SoleSM <miliserrrano@gmail.com>
2022-10-16 10:45:43 -0300

    modificacion de codigo

--- src/controllers/materia.controllers.js
+++ src/controllers/materia.controllers.js
@@ -1,6 +1,6 @@
 const ctrlMateria = {};
 const Materia = require('../models/materias.models');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 //Mostrar todas las materias
 ctrlMateria.getMateria = async (req, res) => {
@@ -8,36 +8,35 @@ ctrlMateria.getMateria = async (req, res) => {
     res.json(materia)
 };
 
-
+//Traer las notas de un alumno enviando por parametros el id de la materia
 ctrlMateria.getNotaAlumno = async (req, res) => {
-    const { id } = req.params.id;
-    const { idAlumno } = req.body;
+    const { id } = req.params;
+    const { alumno } = req.body;
 
     try {
-        const MateriaEncontrada = await Materia.findOne({ id, notas: { $elemMatch: { alumno: idAlumno } } })
-        const { notas } = MateriaEncontrada
-        const Notas = notas.filter(element => element.alumno == idAlumno)
+        
+        const NotasDeAlumno = await Materia.aggregate([
+            {$match: {_id: new mongoose.Types.ObjectId(id)}},
+            {$unwind: "$notas"},
+            {$match: {"notas.alumno" : new mongoose.Types.ObjectId(alumno)}},
+            {$project: {"notas._id": 0, "profesores": 0, "alumnos": 0, "inasistencias": 0, "__v": 0}}
+        ])
 
-        //    const NotaFinal  =  Nota.filter((data, key) => {
-        //         const { alumno } = data
-        //         if(alumno == idAlumno) {
-        //             return Nota[key].calificacion;
-        //         }
-        //     })
-
-        console.log(Notas)
+        console.log(NotasDeAlumno)
         res.json({
-            msg: "Materia encontrada",
-            Notas
+            msg: "Notas encontradas",
+            NotasDeAlumno
         })
     } catch (error) {
-        res.json({
-            msg: "Error Alumno NO encontrado",
+        res.status(400).json({
+            msg: "Error al encontrar las notas de este alumno",
             error
         })
+        console.log(error)
     }
 };
 
+//Traer todas las inasistencias de una materia 
 ctrlMateria.getInasistenciasDia = async (req, res) => {
     const { id } = req.params;
 
@@ -47,8 +46,6 @@ ctrlMateria.getInasistenciasDia = async (req, res) => {
             {$unwind: "$inasistencias"},
             {$project: {"inasistencias.fecha": 1, "inasistencias.alumnos": 1}}
         ])
-
-       console.log(inasistencias)
        res.json(inasistencias)
     } catch (error) {
         res.json(error)
@@ -57,9 +54,10 @@ ctrlMateria.getInasistenciasDia = async (req, res) => {
 
 }
 
+//Total de inasistencias de un alumno en una materia (enviada por parametro)
 ctrlMateria.getCountInasistencias = async (req, res) => {
-
     const { id, idAlumno } = req.params;
+
     try {
         const totalInasistencias = await Materia.aggregate([
             { $match: {_id : new mongoose.Types.ObjectId(id)}},
@@ -68,7 +66,6 @@ ctrlMateria.getCountInasistencias = async (req, res) => {
             { $count: "Cantidad de inasistencias en esta materia"}
     
         ])
-        
         res.json(totalInasistencias)
     } catch (error) {
         res.json(error);
@@ -77,31 +74,45 @@ ctrlMateria.getCountInasistencias = async (req, res) => {
    
 }
 
+//Agregar inasistencia
+ctrlMateria.putInasistencia = async (req, res) => {
+    const { id } = req.params;
+    const { inasistencias } = req.body;
+    try {
+      const ArregloInaActualizado = await Materia.updateOne(
+        {_id: id},
+        {$push: {"inasistencias": inasistencias}}
+      )
+
+       res.json(ArregloInaActualizado)
+    } catch (error) {
+        res.json(error)
+    }
+}
+
+//Modificar nota de alumno
 ctrlMateria.putNotaDeAlumno = async (req, res) => {
+    const { idNota } = req.params;
+    const { calificacion, tipo, periodo } = req.body;
 
     try {
-        const { id, idNota } = req.params;
-        const { calificacion, tipo, periodo } = req.body;
-        const MateriaEncontrada = await Materia.findOne({ id })
-       
-        MateriaEncontrada.notas.map(nota => {
-            if(nota._id == idNota){
-                nota.calificacion = calificacion
-                nota.tipo = tipo
-                nota.periodo = periodo
-            }
-        })
-
-        const materiaModificada = await Materia.findByIdAndUpdate(id, MateriaEncontrada)
-        console.log(materiaModificada)
-        return res.json(materiaModificada)
+        const NotaActualizada = await Materia.updateOne({'notas._id': idNota},
+        {$set: {
+            "notas.$.calificacion": calificacion,
+            "notas.$.tipo": tipo,
+            "notas.$.periodo": periodo
+        }})
+        console.log(NotaActualizada)
+        res.json(NotaActualizada)
     } catch (error) {
         res.json({
             "error": error
         })
+        console.log(error)
     }
 }
 
+//Crear una Materia
 ctrlMateria.postMateria = async (req, res) => {
     const { nombreMateria, profesores, alumnos, inasistencias, notas } = req.body;
 
@@ -120,6 +131,7 @@ ctrlMateria.postMateria = async (req, res) => {
     }
 }
 
+//Modificar materia
 ctrlMateria.putMateria = async (req, res) => {
     const { id } = req.params;
     const { ...resto } = req.body;
@@ -137,19 +149,6 @@ ctrlMateria.putMateria = async (req, res) => {
     }
 }
 
-ctrlMateria.putInasistencia = async (req, res) => {
-    const { id } = req.params;
-    const { inasistencias } = req.body;
-    try {
-      const variable = await Materia.updateOne(
-        {_id: id},
-        {$push: {"inasistencias": inasistencias}}
-      )
 
-       res.json(variable)
-    } catch (error) {
-        res.json(error)
-    }
-}
 
 module.exports = ctrlMateria;
